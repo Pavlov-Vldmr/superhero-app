fa91280858ed744e1de6eb09747d83cc6be19c08
Pavlov-Vldmr <vpavlov95007@gmail.com>
2024-03-04 16:47:35 +0300

    refactor: card container styles. empty styles delete

--- src/components/CardContainer/CardContainer.tsx
+++ src/components/CardContainer/CardContainer.tsx
@@ -1,18 +1,18 @@
 import { Container } from "@mui/system";
-import { experimentalStyled as styled } from '@mui/material/styles';
-import Box from '@mui/material/Box';
-import Paper from '@mui/material/Paper';
+// import { experimentalStyled as styled } from '@mui/material/styles';
+// import Box from '@mui/material/Box';
+// import Paper from '@mui/material/Paper';
 import Grid from '@mui/material/Unstable_Grid2';
 // import ProductItem from "../../components/ProductItem/ProductItem";
 import CardItem from "../CardItem/CardItem";
 
-const Item = styled(Paper)(({ theme }) => ({
-    backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#181D1E',
-    ...theme.typography.body2,
-    padding: theme.spacing(2),
-    textAlign: 'center',
-    color: theme.palette.text.secondary,
-}));
+// const Item = styled(Paper)(({ theme }) => ({
+//     backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#181D1E',
+//     ...theme.typography.body2,
+//     padding: theme.spacing(2),
+//     textAlign: 'center',
+//     color: theme.palette.text.secondary,
+// }));
 
 
 
