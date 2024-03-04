eca99256264f1c9bffd360f65342288eb99d4857
Pavlov-Vldmr <vpavlov95007@gmail.com>
2024-03-04 10:19:22 +0300

    refactor: kebab-case pages dir refactoring

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
-import { HomePage } from './pages/HomePage'
-import { SearchPage } from './pages/SearchPage'
-import { HistoryPage } from './pages/HistoryPage'
-import { FavoritesPage } from './pages/FavoritesPage'
-import { SignupPage } from './pages/SignupPage'
-import { SigninPage } from './pages/SigninPage'
-import { ProductPage } from './pages/ProductPage'
+import { HomePage } from './pages/home-page'
+import { SearchPage } from './pages/search-page'
+import { HistoryPage } from './pages/history-page'
+import { FavoritesPage } from './pages/favorites-page'
+import { SignupPage } from './pages/sign-up-page'
+import { SigninPage } from './pages/sign-in-page'
+import { ProductPage } from './pages/product-page'
 
 
 import './App.css'
