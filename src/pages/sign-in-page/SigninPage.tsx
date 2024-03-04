916530e6ba5748dba7aa1c8f7da2cf20ae53ccb6
Pavlov-Vldmr <vpavlov95007@gmail.com>
2024-03-04 11:42:45 +0300

    feat: add sign in form. TODO: validation

--- src/pages/sign-in-page/SigninPage.tsx
+++ src/pages/sign-in-page/SigninPage.tsx
@@ -1,9 +1,133 @@
-function SigninPage() {
+import * as React from 'react';
+import { styled } from '@mui/material/styles';
+// import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+// import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+// import FormControlLabel from '@mui/material/FormControlLabel';
+// import Checkbox from '@mui/material/Checkbox';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+// import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+// import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+// function Copyright(props: any) {
+//   return (
+//     <Typography variant="body2" color="text.secondary" align="center" {...props}>
+//       {'Copyright © '}
+//       <Link color="inherit" href="https://mui.com/">
+//         Your Website
+//       </Link>{' '}
+//       {new Date().getFullYear()}
+//       {'.'}
+//     </Typography>
+//   );
+// }
+
+// TODO remove, this demo shouldn't need to reset the theme.
+// const defaultTheme = createTheme();
+
+export default function SigninPage() {
+    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        const data = new FormData(event.currentTarget);
+        console.log({
+            email: data.get('email'),
+            password: data.get('password'),
+        });
+    };
+
+    const CssTextField = styled(TextField)({
+        '& .MuiFormLabel-root': {
+            color: '#ffffff88'
+        },
+        '& label.Mui-focused': {
+            color: '#ffffff55'
+        },
+        '& .MuiInput-underline:after': {
+            borderBottomColor: '#B2BAC2',
+        },
+        '& .MuiOutlinedInput-root': {
+            '& fieldset': {
+                borderColor: 'gray',
+            },
+            '&:hover fieldset': {
+                borderColor: '#ffffff22'
+            },
+            '&.Mui-focused fieldset': {
+                borderColor: '#ffffff',
+            },
+        },
+    });
+
+    // TODO: ADD VALIDAtION!
+
     return (
-        <>
-            <code>Signin page</code>
-        </>
+        // <ThemeProvider theme={defaultTheme}>
+        <Container component="main" maxWidth="xs">
+            <Box
+                sx={{
+                    marginTop: 8,
+                    display: 'flex',
+                    flexDirection: 'column',
+                    alignItems: 'center',
+                }}
+            >
+                <Typography component="h1" variant="h5">
+                    Sign in
+                </Typography>
+                <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+                    <CssTextField
+                        margin="normal"
+                        required
+                        fullWidth
+                        id="email"
+                        label="Email Address"
+                        name="email"
+                        autoComplete="email"
+                        autoFocus
+                    />
+                    <CssTextField
+                        margin="normal"
+                        required
+                        fullWidth
+                        name="password"
+                        label="Password"
+                        type="password"
+                        id="password"
+                        autoComplete="current-password"
+
+                    />
+                    {/* <FormControlLabel
+                        control={<Checkbox value="remember" color="primary" />}
+                        label="Remember me"
+                    /> */}
+                    <Button
+                        type="submit"
+                        fullWidth
+                        variant="contained"
+                        sx={{ mt: 3, mb: 2 }}
+                    >
+                        Sign In
+                    </Button>
+                    <Grid container justifyContent={'flex-end'}>
+                        {/* <Grid item xs>
+                            <Link href="#" variant="body2">
+                                Forgot password?
+                            </Link>
+                        </Grid> */}
+                        <Grid item>
+                            <Link href="/signup" variant="body2" color={'#ffffff33'}>
+                                {"Don't have an account? Sign Up"}
+                            </Link>
+                        </Grid>
+                    </Grid>
+                </Box>
+            </Box>
+        </Container>
+        // </ThemeProvider>
     );
 }
-
-export default SigninPage;
